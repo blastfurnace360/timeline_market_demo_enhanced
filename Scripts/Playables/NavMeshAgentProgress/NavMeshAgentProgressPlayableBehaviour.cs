b5612745d20f5e552f77459aa89672a46834dc75
jo <blastfurnace360@gmail.com>
2022-12-30 15:19:22 -0700

    initial commit

---
+++ Scripts/Playables/NavMeshAgentProgress/NavMeshAgentProgressPlayableBehaviour.cs
@@ -0,0 +1,314 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.AI;
+using UnityEngine.Timeline;
+using System;
+using System.Linq;
+
+
+// weird workaround required - without OnAnimatorMove callback timeline NavAgent playback is overriden by root motion animation
+// related to https://forum.unity.com/threads/how-to-properly-use-onanimatormove-without-breaking-timeline.600199/
+public class AnimatorOverride : MonoBehaviour
+{
+	public void OnAnimatorMove() {}
+}
+
+
+[System.Serializable]
+public class NavMeshAgentProgressPlayableBehaviour : PlayableBehaviour
+{
+
+	[HideInInspector]
+	public TimelineClip clip;
+	public NavMeshAgent agent;
+	public Transform startTransform;
+	public Transform targetTransform;
+	public int trackId;
+	public bool updatePathPerFrame = false;
+	public AnimationCurve progressCurve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 1));
+	
+	private GameObject agentGameObject;
+	private float updateRate = 0.01f;
+	private double timeOffset = 0;
+	private bool forwardInTime = true;
+	private float rotationSpeed = 12.0f;
+	private float agentPathProgress;
+	private double lastTime;
+	private float oldDistance = 0.0f;
+	private float distanceTotal = 0.0f;
+	private float elapsed = 0.0f;
+	private float clipProgress = 0.0f;
+	private double clipDuration = 0;
+	
+	private NavMeshPath pathForward;
+	private NavMeshPath pathPast;
+	private Vector3[] cachePathForward;
+	private Vector3[] cachePathPast;
+	private Vector3[] lastPathForwardArray;
+	private Vector3[] lastPathPastArray;
+	
+	public override void OnBehaviourPlay(Playable playable, FrameData info)
+	{
+		//Debug.Log("OnBehaviorPlay called");
+		LoadClipData(playable, info);
+		InitialSetUp(playable, info);
+	}
+	
+	public void OnBehaviourPause(Playable playable, FrameData info)
+	{
+		ClearCache();
+	}
+	
+	public override void OnGraphStop(Playable playable)
+	{
+		// weird workaround required - without OnAnimatorMove callback timeline NavAgent playback is overriden by root motion animation
+		// related to https://forum.unity.com/threads/how-to-properly-use-onanimatormove-without-breaking-timeline.600199/	
+		//Debug.Log("OnGraphStop called");
+		if (agentGameObject != null && agentGameObject.GetComponent<AnimatorOverride>() != null)
+			GameObject.DestroyImmediate(agentGameObject.GetComponent<AnimatorOverride>());
+	}
+	
+	private void LoadClipData(Playable playable, FrameData info)
+	{
+		var playableDirector = (playable.GetGraph().GetResolver() as PlayableDirector);
+		var timelineAsset = playableDirector.playableAsset as TimelineAsset;
+		
+		foreach (var track in timelineAsset.GetOutputTracks())
+		{
+			var navMeshAgentProgressTrack = track as NavMeshAgentProgressTrack;
+			if (navMeshAgentProgressTrack != null)
+			{
+				foreach (TimelineClip timelineClip in navMeshAgentProgressTrack.GetClips())
+				{
+					var asset = timelineClip.asset as NavMeshAgentProgressClip;
+				
+					if (asset)
+						if (asset.trackId == trackId)
+						{
+							UnityEngine.Object binding = playableDirector.GetGenericBinding(track);
+							clip = timelineClip;
+							agent = binding as NavMeshAgent;
+						}
+				}
+			}
+		}
+	}
+	
+	private void InitialSetUp(Playable playable, FrameData info)
+	{
+		if (agent == null || startTransform == null || targetTransform == null)
+			return;
+		
+		Vector3[] pathForwardArray = UpdatePathForward(agent.transform.position, targetTransform.position, forwardInTime, lastPathForwardArray, false);
+		Vector3[] pathPastArray = UpdatePathPast(agent.transform.position, startTransform.position, forwardInTime, lastPathPastArray, false);
+		float distanceForward = PathDistance(pathForwardArray);
+		float distancePast = PathDistance(pathPastArray);
+		
+		oldDistance = distanceTotal = distanceForward + distancePast;
+		
+		timeOffset = 0;
+		agentGameObject = agent.gameObject;
+		
+		// weird workaround required - without OnAnimatorMove callback timeline NavAgent playback is overriden by root motion animation
+		// related to https://forum.unity.com/threads/how-to-properly-use-onanimatormove-without-breaking-timeline.600199/
+		if (agentGameObject.GetComponent<AnimatorOverride>() == null)
+			agentGameObject.AddComponent<AnimatorOverride>();
+	}
+	
+	private void ClearVector3Array(Vector3[] vector3Array)
+	{
+		if (vector3Array != null)
+			Array.Clear(vector3Array, 0, vector3Array.Length);
+	}
+	
+	private void ClearCache()
+	{
+		elapsed = 0.0f;
+		updateRate = 0.01f;
+		pathForward = null;
+		pathPast = null;
+		timeOffset = 0;
+		forwardInTime = true;
+		rotationSpeed = 12;
+		agentPathProgress = 0;
+		lastTime = 0;
+		oldDistance = 0;
+		distanceTotal = 0;
+		clipProgress = 0;
+		clipDuration = 0;
+		ClearVector3Array(cachePathForward);
+		ClearVector3Array(cachePathPast);
+		ClearVector3Array(lastPathForwardArray);
+		ClearVector3Array(lastPathPastArray);	
+	}
+	
+	public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+	{
+		agent = playerData as NavMeshAgent;
+	}
+	
+	public override void PrepareFrame(Playable playable, FrameData info)
+	{
+		if (!Mathf.Approximately(distanceTotal, oldDistance))
+			timeOffset = playable.GetTime();
+			
+		clipDuration = playable.GetDuration();
+		clipProgress = (float)(playable.GetTime() - timeOffset) / (float)(clipDuration - timeOffset);
+		
+		forwardInTime = lastTime < playable.GetTime() ? true : false;
+		
+		// evaluate the curve using the normalised time of the clip
+		agentPathProgress = progressCurve.Evaluate(clipProgress);
+		
+		lastTime = playable.GetTime();
+		if (agent == null || targetTransform == null)
+			return;
+		
+		Vector3[] pathForwardArray = UpdatePathForward(agent.transform.position, targetTransform.position, forwardInTime, lastPathForwardArray, false);
+		lastPathForwardArray = pathForwardArray;
+		Vector3[] pathPastArray = UpdatePathPast(agent.transform.position, startTransform.position, forwardInTime, lastPathPastArray, false);
+		lastPathPastArray = pathPastArray;
+		
+		Transform newTransform = CalculateTransformAlongPath(agentPathProgress, pathForwardArray, pathPastArray, forwardInTime); 
+		agent.transform.position = newTransform.position;
+		agent.transform.rotation = newTransform.rotation;
+
+		oldDistance = distanceTotal;
+	}
+	
+	private Vector3[] UpdatePathForward(Vector3 startPosition, Vector3 endPosition, bool _forwardInTime, Vector3[] lastPathForward, bool _recalculate = false)
+	{
+		Vector3[] cornersForward = new Vector3[] { };
+		// Checks if NavMesh path calculation is valid this frame
+		bool canCalculatePathForward = true;
+		
+		if (pathForward == null)
+			pathForward = new NavMeshPath();
+			
+		if (updatePathPerFrame || cornersForward.Length <= 0 || _recalculate)
+		{
+			elapsed += Time.deltaTime;
+			if (elapsed > updateRate)
+			{
+				elapsed -= updateRate;
+				canCalculatePathForward = NavMesh.CalculatePath(startPosition, endPosition, NavMesh.AllAreas, pathForward);
+			}
+		}
+		
+		cachePathForward = pathForward.corners;
+		cornersForward = cachePathForward;
+		NavMeshHit hit;
+		
+		// Return calulated path if NavAgent is on viable NavMesh position, else return last good calculated path
+		// This is to prevent the NavAgent from getting stuck
+		return !canCalculatePathForward ? lastPathForward : cornersForward;
+	}
+	
+	private Vector3[] UpdatePathPast(Vector3 startPos, Vector3 endPos, bool _forwardInTime, Vector3[] lastPathPast, bool _recalculate = false)
+	{
+		Vector3[] cornersPast = new Vector3[] { };
+		
+		bool canCalculatePathPast = true;
+		
+		if (pathPast == null)
+			pathPast = new NavMeshPath();
+		
+		if (updatePathPerFrame || cornersPast.Length <= 0 || _recalculate)
+		{
+			elapsed += Time.deltaTime;
+			if (elapsed > updateRate)
+			{
+				elapsed -= updateRate;
+				canCalculatePathPast = NavMesh.CalculatePath(startPos, endPos, NavMesh.AllAreas, pathPast);
+			}
+		}
+		
+		cachePathPast = pathPast.corners;
+		cornersPast = cachePathPast;
+		NavMeshHit hit;
+		
+		return !canCalculatePathPast ? lastPathPast : cornersPast;
+	}
+	
+	// this is called every frame - in timeline preview you may be progressing forward or backward in time
+	// depending on the direction of time, the point the NavAgent is moving towards is found and the clip progress is used to
+	// find the exact location the NavAgent should be. Rotation towards or away from that point is given depending on the direction of time
+	
+	private Transform CalculateTransformAlongPath(float progress, Vector3[] pathForward, Vector3[] pathPast, bool _forward)
+	{
+		Transform newTransform = agent.transform;
+		Vector3 newDirection = agent.transform.forward;
+		
+		float distanceForward = PathDistance(pathForward);
+		float distancePast = PathDistance(pathPast);
+		
+		DrawColoredLine(pathPast, Color.red);
+		DrawColoredLine(pathForward, Color.blue);
+		
+		distanceTotal = distanceForward + distancePast;
+		
+		// Change progress percentage and path based on direction of time
+		float progressPercent = _forward ? 1 - progress : progress;
+		Vector3[] newDirectionPath =  _forward ? pathForward : pathPast;
+		
+		float pathPointsDistance = 0;
+		
+		// the path being analyzed is iterated over backwards (closest to furthest) so the points ahead and behind NavAgent's location can be found
+		// it's highly likely that the first few points will be the ones that the NavAgent is between
+		for (int i = newDirectionPath.Length - 1; i >= 1; i--)
+		{
+			// accumulate the distance we've analyzed so far
+			pathPointsDistance += Vector3.Distance(newDirectionPath[i], newDirectionPath[i - 1]);
+			
+			float pathPointAheadDistance = pathPointsDistance;
+			// distance the agent is along the whole path
+			float agentProgress = progressPercent * distanceTotal;
+			
+			// the first time we find a point ahead of the NavAgent, we know the NavAgent is between [i] and [i - 1]
+			// remember time may be progressing backwards using timeline preview, so past and forward just means in respect to the next frame
+			if (pathPointAheadDistance > agentProgress)
+			{
+				Vector3 pointPast = newDirectionPath[i];
+				Vector3 pointForward = newDirectionPath[i - 1];
+				
+				float pathPointBehindDistance = pathPointsDistance - Vector3.Distance(pointPast, pointForward);
+				// the distance the NavAgent is ahead of the point behind
+				float agentProgressDistance = agentProgress - pathPointBehindDistance;
+				// get the direction between the two points
+				Vector3 directionToTravel = pointForward - pointPast;
+				// normalize the direction so it's between -1 and 1
+				directionToTravel.Normalize();
+				// the new position for the NavAgent ahead of the point behind the NavAgent depending on its progress
+				Vector3 newPosition = pointPast + (directionToTravel * agentProgressDistance);
+				newTransform.position = Vector3.Lerp(agent.transform.position, newPosition, agentPathProgress / (float)clipDuration);
+				// depending on the direction of time the NavAgent will face towards or away from the end of the path we analyzed
+				newDirection = _forward ? -directionToTravel : directionToTravel;
+				newTransform.rotation = Quaternion.Slerp(agent.transform.rotation, Quaternion.LookRotation(newDirection), Time.deltaTime * rotationSpeed);
+			}
+		}
+		return newTransform;
+	}
+	
+	private float PathDistance(Vector3[] _path)
+	{
+		float totalDistance = 0;
+		Vector3[] points = _path;
+		
+		for (int i = 0; i < points.Length - 1; i++)
+		{
+			totalDistance += Vector3.Distance(points[i], points[i + 1]);
+		}
+		
+		return totalDistance;
+	}
+	
+	private void DrawColoredLine(Vector3[] points, Color color)
+	{
+		for (int i = 0; i < points.Length - 1; i++)
+		{
+			Debug.DrawLine(points[i], points[i + 1], color);
+		}
+	}
+}
---
+++ readme.md
@@ -0,0 +1,56 @@
+# Timeline Market Demo Enhanced  
+
+An updated version of the project files from the 'Unite Europe 2017 - Use Timeline and Cinemachine to mix gameplay & cutscenes' talk. It has been updated from the original to use the latest version of cinemachine, textmesh pro, URP, and has enhanced playables such as one that allows for you to use NavMesh paths on timeline with scrubbing and dynamic adjustments.   
+
+![market demo screenshot](.github/market_demo_screenshot.png?raw=true "market demo screenshot")  
+
+## Background  
+
+The original 'timeline market demo' with all of the playables used in the talk isn't available anymore from Unity - probably because it's so old now that it won't work without overhaul. I found the assets elsewhere on github and fix/reorganized/enhanced it.  
+
+Repo where I found the assets (with some useful playables!):  
+
+[https://github.com/PlayApple/UnityTimelineExample](https://github.com/PlayApple/UnityTimelineExample)    
+
+Repo where I found the beautiful URP toon shader:  
+
+[https://github.com/ardahamamcioglu/ToonShader_URP](https://github.com/ardahamamcioglu/ToonShader_URP)  
+
+Other excellent repo with cinemachine demonstrations and adventure game assets:  
+
+[https://github.com/ciro-unity/cinemachine-market](https://github.com/ciro-unity/cinemachine-market)  
+
+I fixed everything that was broken so it can continue to be used for learning about timeline, cinemachine and the playables demostrated in the talk.  
+
+## Steps to Follow to Run the Project  
+
+-- Create a new URP project  
+-- Install Cinemachine from the package manager (click window -> package manager -> unity registry -> find cinemachine and click 'install'  
+-- Install TextMeshPro using the integrated unity menu (click window -> textmesh pro -> import TMP Essential Resources  
+-- clone or download this repo and put everything into your project's Asset folder  
+-- find Scenes/demo and open it, click play button  
+-- press 'V' key while in playmode to spawn some collectables to see how they work  
+-- enable the DayAndNightCycle and StormTimeline GameObjects in the scene to see how what they do  
+
+## Notes on Contents and NavMeshAgentProgress playable  
+
+Find the Unite Europe 2017 talk by Andy Touch on youtube for an explanation of the project.    
+The original adventure game project (where all of the models/artwork came from) had a toon surface shader on all of the materials which can't render in URP. It can be downloaded free from the unity asset store.   
+
+The conversations playable has been enhanced to use TMP_Text and TMP_FontAsset instead of the older UI elements. It was also enhanced by PlayApple to display character images.  
+
+The GuardTimeline is using a custom NavMeshAgentProgress playable which is different from the older NavMeshAgentControl playable available with the free default playables and included in this repo. The original playable allows for you to set a destination and only works in play mode. The NavMeshAgentProgress playable does a lot more than that.  
+
+The NavMeshAgentProgress playable has preview/scrubbing in the timeline window. It does this by converting the timeline clip progress to a position on a NavMeshAgent's path. You are intended to have a separate animation track with the walking animation and others as shown in the example.  
+
+One thing to be aware of is that root motion curves in the animations can interfere with the path movement. I discovered a workaround to this that's mentioned here  
+
+[Scripts/Playables/NavMeshAgentProgress/NavMeshAgentProgressPlayableBehaviour.cs#L123](Scripts/Playables/NavMeshAgentProgress/NavMeshAgentProgressPlayableBehaviour.cs#L123)    
+
+The playable adds a component to the NavMeshAgent's GameObject that's has an empty OnAnimatorMoved callback and removes it when the timeline is done. No clue why that fixes the problem but if you remove that part you'll see the issue I'm talking about.  
+
+Like it says in the comment, this has something to do with what's described in this forum thread.  
+
+[https://forum.unity.com/threads/how-to-properly-use-onanimatormove-without-breaking-timeline.600199/](https://forum.unity.com/threads/how-to-properly-use-onanimatormove-without-breaking-timeline.600199/)  
+
+I hope this NavMesh path progression playable is useful to people who clone this project!  
